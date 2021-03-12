4ab3d722d55fff1af399a219506295216ec30cdf
demiroglubugra <bugra.demiroglu@hotmail.com>
2021-03-13 01:37:32 +0300

    deploying to amazon ec2 instance

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2*#lbau^x02o%85lds8g51w@2)f)7ki=4!3c+mv(8+_pcp=ur_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
